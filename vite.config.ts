42dbf91db3446d91cb360e206a5d20f1f4a84acd
Hank Goddard <me@henrygoddard.com>
2026-01-09 12:37:33 -0500

    chore: remove the base as we're using a custom domain on the root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/energlad-2026-v1/",
   server: {
     host: "::",
     port: 8080,
